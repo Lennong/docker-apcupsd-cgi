3f5ec23ce8f1b5f5b8a88f85f2609e421b71149b
Lennong <tibblin@hotmail.com>
2024-05-06 21:54:09 +0200

    Update multi-arch-build.sh

--- multi-arch-build.sh
+++ multi-arch-build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -f Dockerfile -t bnhf/apcupsd-cgi . --push --no-cache
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -f Dockerfile -t lennong05/apcupsd-cgi . --push --no-cache
