9c34b31984cdd75a931923bdc2a7d67b3a37e4f7
Lennong <tibblin@hotmail.com>
2024-05-06 21:52:37 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -20,6 +20,6 @@ done
 # change the IP to monitor with the host IP in /etc/apcupsd/hosts.conf
 # sed -i 's/MONITOR 127.0.0.1/MONITOR \"$hostIp\"/g' /etc/apcupsd/hosts.conf
 
-# start fcgiwarap
+# start fcgiwrap
 /etc/init.d/fcgiwrap start
 nginx -g 'daemon off;'
