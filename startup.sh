f20969b315053836847dda25087922dabc326534
Lennong <tibblin@hotmail.com>
2024-05-07 07:59:56 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 # remove sample and create empty hosts file
-rm /etc/apcupsd/hosts.conf && touch /etc/apcupsd/hosts.conf
+#rm /etc/apcupsd/hosts.conf && touch /etc/apcupsd/hosts.conf
 
-# copy config files
-cp /opt/multimon.conf /etc/apcupsd/multimon.conf
-cp /opt/apcupsd.css /etc/apcupsd/apcupsd.css
+# copy config files if not already exists/configured
+cp -n /opt/hosts.conf /etc/apcupsd/hosts.conf
+cp -n /opt/multimon.conf /etc/apcupsd/multimon.conf
+cp -n /opt/apcupsd.css /etc/apcupsd/apcupsd.css
 
-# populate two arrays with host and UPS names
+# if no MONITOR case has been configured, 
+# populate two arrays and add monitors to hosts.conf for each host and UPS name combo
+if ! grep -w "^MONITOR" < "/etc/apcupsd/hosts.conf" > /dev/null; then
 HOSTS=( $UPSHOSTS )
 NAMES=( $UPSNAMES )
-
-# add monitors to hosts.conf for each host and UPS name combo
 for ((i=0;i<${#HOSTS[@]};i++))
     do
         echo "MONITOR ${HOSTS[$i]} \"${NAMES[$i]}\"" >> /etc/apcupsd/hosts.conf
         echo "MONITOR ${HOSTS[$i]} \"${NAMES[$i]}\""
 done
+fi
 
 # start fcgiwrap
 /etc/init.d/fcgiwrap start
